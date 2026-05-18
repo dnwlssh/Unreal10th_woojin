1520e843d7e7e5423d4f83c36acf32f58a3c27d2
우진 노 <woojin8924@naver.com>
2026-05-18 11:31:56 +0900

    컨플릿트 테스트

--- Unreal10th_woojin/Unreal10th_woojin.cpp
+++ Unreal10th_woojin/Unreal10th_woojin.cpp
@@ -8,7 +8,7 @@ int main()
     std::cin >> Age;
     printf("제 나이는 [%d]살 입니다.", Age);
 
-    printf("Hello Branch Branch!\n");
+    printf("안녕하세요 Git Branch\n");
 
     // 변수(Variable)
     // - 변하는 숫자
